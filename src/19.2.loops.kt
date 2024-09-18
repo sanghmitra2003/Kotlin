f6779b4ab68c9ac39976994ed96245fcf2cf2fa9
sanghmitra2003 <kandpalsanghmitra@gmail.com>
2024-09-18 19:10:56 +0530

    kotlin added

---
+++ src/19.2.loops.kt
@@ -0,0 +1,10 @@
+fun main()
+{
+    var number=2
+    var index= 1
+    while(index<=10)
+    {
+        println(number*index)
+        index++
+    }
+}
---
+++ src/19Loops.kt
@@ -0,0 +1,18 @@
+//Loops
+//execute set of instructions multiple times
+fun main()
+{
+    var count = 5
+    while(count>=1)
+    {
+        println("hello")
+        count--
+    }
+}
+
+
+
+
+
+
+
